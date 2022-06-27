4c0e3f5804aae95169792a60ee29ac041957bb3c
devongeoffreymaina@gmail.com <devongeoffreymaina@gmail.com>
2022-06-27 10:44:31 -0700

    Added closing bracket to 7 edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -5,4 +5,4 @@ word_last_2 = word[-2:]
 middle_word = word[1:-1]
 print(f"First 3 letters: {word_first[:3]}")
 print(f"Last 2 letters: {word_last[-2:]}")
-print(f"Middle word: {middle_word[1:-1}")
+print(f"Middle word: {middle_word[1:-1]}")
