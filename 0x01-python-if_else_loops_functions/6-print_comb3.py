282bf2778b028ce452d6e539de13f3ad796d7561
devongeoffreymaina@gmail.com <devongeoffreymaina@gmail.com>
2022-06-28 08:15:55 -0700

    Fix 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@
 for i in range(0, 90):
     if i == 89:
         print("{}".format(i))
-    elif (i % 11 != 0) & (i % 10 != 0):
+    elif (i % 11 != 0) & (i % 10 != 0) & (i % 10 != 1):
         print("{:02}".format(i), end=", ")
