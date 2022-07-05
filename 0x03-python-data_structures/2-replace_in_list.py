27c0f6d005b0550163272f9b64551e885119f617
devongeoffreymaina@gmail.com <devongeoffreymaina@gmail.com>
2022-07-05 04:03:39 -0700

    fix errors for task2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    new_element = element
-
     for i in range(len(my_list)):
         if idx < 0 or idx > (len(my_list)):
             return my_list
         else:
+            new_element = element
             my_list[idx] = new_element
             new_list = my_list
             return new_list
-
