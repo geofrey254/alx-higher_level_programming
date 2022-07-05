a46e780d0f8f9efea89846a24c27bfbdce7dd6c2
devongeoffreymaina@gmail.com <devongeoffreymaina@gmail.com>
2022-07-05 04:01:48 -0700

    Defined new element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
+    new_element = element
+
     for i in range(len(my_list)):
-        if idx < 0 or idx > (len(my_list - 1)):
+        if idx < 0 or idx > (len(my_list)):
             return my_list
         else:
             my_list[idx] = new_element
