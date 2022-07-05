9f61d095248655d750d3393a14b68763e0f9c93a
devongeoffreymaina@gmail.com <devongeoffreymaina@gmail.com>
2022-07-05 11:04:00 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for num in i:
+            if num in i[:-1]:
+                print("{:d}".format(num), end=" ")
+            else:
+                print("{:d}".format(num), end=" ")
+
+        print("")
